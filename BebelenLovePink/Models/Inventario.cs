d196be5ba887dfa56aeab62aa9dc9a056a915840
Halito33 <alexander.j.figueroa.v@gmail.com>
2024-11-25 21:34:10 -0300

    agregado modelo tabla para vista inventario(en proceso)

--- BebelenLovePink/Models/Inventario.cs
+++ BebelenLovePink/Models/Inventario.cs
@@ -1,10 +1,11 @@
 ﻿namespace BebelenLovePink.Models
 {
-    public class Inventario
+    public partial class Inventario
     {
             public int id { get; set; }
             public string name { get; set; }
             public string description { get; set; }
+            public string photo { get; set; }
             public int quantities { get; set; }
     }
 }
